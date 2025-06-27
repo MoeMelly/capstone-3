3658a5055fc3358066e3e2674070087fd26aebd8
Student <mkomi@appdev.yearup.org>
2025-06-26 23:22:18 -0500

    Testing phase...Postman tests to ensure robustness and correctly working methods.

--- backend/src/main/java/org/yearup/controllers/CategoriesController.java
+++ backend/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -35,8 +36,14 @@ public class CategoriesController {
 
     @GetMapping("/{id}")
     public Category getById(@PathVariable int id)
+
     {
-        return categoryDao.getById(id);
+
+        Category category = categoryDao.getById(id);
+        if (category == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "id not found.");
+        }
+        return category;
     }
 
     // the url to return all products in category 1 would look like this
